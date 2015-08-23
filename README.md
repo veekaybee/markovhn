f44b362d319f4965c6ab7b916b4877309752b286
V <vicki.boykis@gmail.com>
2015-08-22 20:26:33 -0400

    Creating Python script to pull HN titles from API

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
-# markovhn
-Creating Markov chain-generated Hacker News headlines with Python
+# Markov Chain Hacker News Headline Generator
+Creating Markov chain-generated Hacker News headlines with Python. Lots of different implementations out there. Wanted to see what the latest way of pulling Hacker News headlines is. 
---
+++ news.py
@@ -0,0 +1,16 @@
+import urllib2
+import json
+
+#HackerNews API documentation: https://github.com/HackerNews/API
+
+#Pull all story numbers into a Python data dictionary
+response = urllib2.urlopen('https://hacker-news.firebaseio.com/v0/topstories.json')
+data=json.load(response)
+
+#Takes each story number and extracts the title by treating as Python dictionary
+for i in data:
+	genurl='https://hacker-news.firebaseio.com/v0/item/%s.json?print=pretty' % (i) 
+	#print genurl
+	item_response=urllib2.urlopen(genurl)  
+	parsed_response=json.load(item_response)
+	print parsed_response["title"]
