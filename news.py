3a0c4a41d9f77d822e3e90e20f9fe65331c9911a
V <vicki.boykis@gmail.com>
2015-08-24 07:44:49 -0400

    Refactoring news.py, renaming markov for easier ID

--- news.py
+++ news.py
@@ -3,14 +3,19 @@ import json
 
 #HackerNews API documentation: https://github.com/HackerNews/API
 
+api_url='https://hacker-news.firebaseio.com/v0/topstories.json'
+item_url='https://hacker-news.firebaseio.com/v0/item/'
+
 #Pull all story numbers into a Python data dictionary
-response = urllib2.urlopen('https://hacker-news.firebaseio.com/v0/topstories.json')
+response = urllib2.urlopen(api_url)
 data=json.load(response)
 
 #Takes each story number and extracts the title by treating as Python dictionary
-for i in data:
-	genurl='https://hacker-news.firebaseio.com/v0/item/%s.json?print=pretty' % (i) 
-	#print genurl
-	item_response=urllib2.urlopen(genurl)  
-	parsed_response=json.load(item_response)
-	print parsed_response["title"].encode('utf-8')
+with open("headlines.txt", "w") as output_file:
+	for i in data:
+		genurl="%s%s.json?print=pretty" % (item_url, i) 
+		item_response=urllib2.urlopen(genurl)  
+		parsed_response=json.load(item_response)
+		output_file.write(parsed_response["title"].encode('utf-8'))
+
+
